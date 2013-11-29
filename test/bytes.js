05ea9ab45499408b9d2d5f3ff4892bb7c922cb31
James Halliday <mail@substack.net>
2013-11-29 13:24:12 -0800

    test ALL the code points

--- test/bytes.js
+++ test/bytes.js
@@ -5,3 +5,11 @@ test('some canned examples', function (t) {
     t.deepEqual(bytes('[☉,☼]'), Buffer('[☉,☼]').toJSON());
     t.end();
 });
+
+test('all the code points', function (t) {
+    t.plan(65536);
+    for (var i = 0; i < 65536; i++) {
+        var s = String.fromCharCode(i);
+        t.deepEqual(bytes(s), Buffer(s).toJSON());
+    }
+});
