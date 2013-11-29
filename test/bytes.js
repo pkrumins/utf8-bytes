d32ade1bd3be571e1dc5e430336f1d001574ed27
James Halliday <mail@substack.net>
2013-11-29 13:38:37 -0800

    update the tests to run on 0.8

--- test/bytes.js
+++ test/bytes.js
@@ -2,7 +2,7 @@ var bytes = require('../');
 var test = require('tape');
 
 test('some canned examples', function (t) {
-    t.deepEqual(bytes('[☉,☼]'), Buffer('[☉,☼]').toJSON());
+    t.deepEqual(bytes('[☉,☼]'), bufArray('[☉,☼]'));
     t.end();
 });
 
@@ -10,6 +10,10 @@ test('all the code points', function (t) {
     t.plan(65536);
     for (var i = 0; i < 65536; i++) {
         var s = String.fromCharCode(i);
-        t.deepEqual(bytes(s), Buffer(s).toJSON());
+        t.deepEqual(bytes(s), bufArray(s));
     }
 });
+
+function bufArray (s) {
+    return [].slice.call(Buffer(s));
+}
